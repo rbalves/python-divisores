fa335dc3b78bfadafcfbe051a7a432906263d179
Rafael Barbosa Alves <rafael.alves@samsung.com>
2020-03-05 14:23:08 -0400

    General changes

--- calculo.py
+++ calculo.py
@@ -1,16 +1,10 @@
-ler_qtde_funcionarios = lambda: int(input("Quantidade de funcionários: "))
-
 ler_matricula = lambda: int(input())
 
 MENSAGEM_MAIOR_QUE_ZERO = "VALOR PRECISA SER MAIOR QUE 0"
 
 eh_divisor = lambda dividendo, divisor: dividendo % divisor == 0
 
-qtde_funcionarios = ler_qtde_funcionarios()
-
-while qtde_funcionarios < 1:
-    print(MENSAGEM_MAIOR_QUE_ZERO)
-    qtde_funcionarios = ler_qtde_funcionarios()
+qtde_funcionarios = 2
 
 matriculas = []
 
